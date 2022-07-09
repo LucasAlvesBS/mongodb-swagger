6c37e1dc896ea46eafd3e3d94a101e7ba705b610
Lucas Alves <lucasalvesbs6@gmail.com>
2022-07-09 18:35:24 -0300

    refactor: change users folder path

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './app/users/users.module';
+import { SendgridModule } from './app/sendgrid/sendgrid.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     UsersModule,
+    SendgridModule,
   ],
   controllers: [AppController],
   providers: [AppService],
